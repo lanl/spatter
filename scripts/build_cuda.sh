da4aba9fc50291ee80d9e2b550085ec80c4c13a1
Jered Dominguez-Trujillo <jereddt@lanl.gov>
2023-05-01 14:35:09 -0600

    Removed git clone from build_cuda script

--- scripts/build_cuda.sh
+++ scripts/build_cuda.sh
@@ -6,10 +6,9 @@ source ${HOMEDIR}/scripts/config.sh
 cd ${HOMEDIR}
 source ${MODULEFILE}
 
-git clone git@github.com:hpcgarage/spatter.git
 cd spatter
 
-cc=`nvidia-smi --query-gpu=compute_cap --format=csv,noheader|head -n 1 | tr -d '.'`
+cc=`nvidia-smi --query-gpu=compute_cap --format=csv,noheader | head -n 1 | tr -d '.'`
 
 ./configure/configure_cuda ${cc}
 
