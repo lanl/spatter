eb1fcbe840a979a146f731fc754ead17e4a24dae
Jered Dominguez-Trujillo <jereddt@lanl.gov>
2023-05-01 15:21:55 -0600

    Updated mpirunscaling script

--- scripts/mpirunscaling.sh
+++ scripts/mpirunscaling.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 usage() {
-	echo "Usage ./scaling.sh -a <application> -p <input problem> -f <pattern> -n <arch> [-c 'toggle core binding'] [-g <0/1 enable/disable plotting>] [-r 'toggle scaling with MPI'] [-t 'toggle scaling with OpenMP'] [-w 'toggle weak scaling']
+	echo "Usage ./scaling.sh -a <application> -p <input problem> -f <pattern> -n <arch> [-c 'toggle core binding'] [-g <0/1 enable/disable plotting>] [-r 'toggle scaling with MPI'] [-w 'toggle weak scaling']
 		-a : Specify the name of the application (see available apps in the patterns sub-directory)
 		-p : Specify the name of the input problem (see available problems in the application sub-directory)
 		-f : Specify the base name of the input JSON file containing the gather/scatter pattern (see available patterns in the input problem subdirectory)
-		-n : Specify the architecture/partition being ran on (used to appropriately save data)
+		-n : Specify the test name you would like to use to identify this run (used to appropriately save data)
 		-c : Optional, toggle core binding (default: off)
 		-g : Optional, toggle plotting/post-processing. Requires python3 environment with pandas, matplotlib (default: on)
 		-r : Optional, enable scaling with MPI (default: off)
-		-t : Optional, enable scaling with OpenMP (default: off)
 		-w : Optional, enable weak scaling (default: off, strong scaling)"	
 }
 
@@ -19,10 +18,9 @@ else
         SCALINGDIR="spatter.scaling"
         WEAKSCALING=0
 	MPI=0
-	OPENMP=0
 	PLOT=1
 	BINDING=0
-	while getopts "a:f:p:n:cgrtw" opt; do
+	while getopts "a:f:p:n:cgrw" opt; do
 		case $opt in 
 			a) APP=$OPTARG
 			;;
@@ -30,15 +28,13 @@ else
 			;;
 			p) PROBLEM=$OPTARG
 			;;
-			n) ARCH=$OPTARG
+			n) RUNNAME=$OPTARG
 			;;
 			c) BINDING=1
 			;;
 			g) PLOT=$OPTARG
 			;;
 			r) MPI=1
-			;;
-			t) OPENMP=1
 			;;
 			w) WEAKSCALING=1
 			;;
@@ -53,7 +49,7 @@ else
 	echo "APP: ${APP}"
 	echo "PROBLEM: ${PROBLEM}"
 	echo "PATTERN: ${PATTERN}"
-	echo "ARCH: ${ARCH}"
+	echo "RUNNAME: ${RUNNAME}"
 	echo "PLOTTING: ${PLOT}"
 
 	echo "MPI SCALING: ${MPI}"
@@ -70,33 +66,20 @@ else
 		echo "BINDING: ${BINDING}"
 	fi
 
-	echo "OPENMP SCALING: ${OPENMP}"
-	if [[ "${OPENMP}" -eq "1" ]]; then
-		echo "THREAD LIST: ${threadlist[*]}"
-	fi
-
 	cd ${HOMEDIR}
 	source ${MODULEFILE}
 
 	# Ensure that we got all the arguments
-	if [[ -z "${APP}" || -z "${PROBLEM}" || -z "${PATTERN}" || -z "${ARCH}" ]] ; then
+	if [[ -z "${APP}" || -z "${PROBLEM}" || -z "${PATTERN}" || -z "${RUNNAME}" ]] ; then
 		usage
 		exit 1
 	fi
 
 	JSON=${HOMEDIR}/patterns/${APP}/${PROBLEM}/${PATTERN}.json
 
-	mkdir -p ${SCALINGDIR}/${ARCH}/${APP}/${PROBLEM}/${PATTERN}
+	mkdir -p ${SCALINGDIR}/${RUNNAME}/${APP}/${PROBLEM}/${PATTERN}
 			
-	cd ${SCALINGDIR}/${ARCH}/${APP}/${PROBLEM}/${PATTERN}
-
-	if [[ "${OPENMP}" -eq "1" ]]; then
-		for thread in ${threadlist[@]}; do
-			export OMP_NUM_THREADS=${thread}
-
-			${SPATTER} -pFILE=${JSON} -q3 > openmp_1r_${thread}t.txt
-		done
-	fi
+	cd ${SCALINGDIR}/${RUNNAME}/${APP}/${PROBLEM}/${PATTERN}
 
 	if [[ "${MPI}" -eq "1" ]]; then
 		export OMP_NUM_THREADS=1
@@ -118,9 +101,9 @@ else
 			echo ${CMD}
 			${CMD} > mpi_${ranklist[i]}r_1t.txt
 
-			if [[ "${WEAKSCALING}" -ne "1" ]]; then
+                        if [[ "${WEAKSCALING}" -ne "1" ]]; then
 				mv ${JSON}.orig ${JSON}
-			fi
+                        fi
 
 			mkdir -p ${ranklist[i]}r
 
@@ -128,16 +111,22 @@ else
 			num_patterns="$((num_patterns-1))"
 
 			for pattern in $(seq 0 ${num_patterns}); do
-				cat mpi_${ranklist[i]}r_1t.txt | grep "^${pattern} " | cut -d$' ' -f 12-18 > ${ranklist[i]}r/${ranklist[i]}r_1t_${pattern}p.txt.tmp
+				cat mpi_${ranklist[i]}r_1t.txt | grep "^${pattern} " | awk '{print $3}' > ${ranklist[i]}r/${ranklist[i]}r_1t_${pattern}p.txt.tmp
 				cat ${ranklist[i]}r/${ranklist[i]}r_1t_${pattern}p.txt.tmp | awk '{$1=$1};1' > ${ranklist[i]}r/${ranklist[i]}r_1t_${pattern}p.txt
 				rm ${ranklist[i]}r/${ranklist[i]}r_1t_${pattern}p.txt.tmp
 			done
 		done
+
+		echo ""
 	
 		if [[ "${PLOT}" -eq "1" ]]; then
 			cd ${HOMEDIR}
-			mkdir -p figures/${ARCH}/${APP}/${PROBLEM}/${PATTERN}
-			python3 scripts/plot_mpi.py ${SCALINGDIR}/${ARCH}/${APP}/${PROBLEM}/${PATTERN} ${ARCH}
+			mkdir -p figures/${RUNNAME}/${APP}/${PROBLEM}/${PATTERN}
+			echo "Plotting Results..."
+			python3 scripts/plot_mpi.py ${SCALINGDIR}/${RUNNAME}/${APP}/${PROBLEM}/${PATTERN} ${RUNNAME}
+			echo ""
 		fi
 	fi
+
+	echo "See ${SCALINGDIR}/${RUNNAME}/${APP}/${PROBLEM}/${PATTERN} for results"
 fi
