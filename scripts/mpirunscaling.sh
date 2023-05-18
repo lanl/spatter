2cb938435c2b977012c2bf615d989efed5379b0e
Jered Dominguez-Trujillo <jereddt@lanl.gov>
2023-05-18 17:11:36 -0600

    Added -g for GPU, -t for throughput plots, changed -g to -x for plotting, added -s for sizing, updated readme

--- scripts/mpirunscaling.sh
+++ scripts/mpirunscaling.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 usage() {
-	echo "Usage ./scaling.sh -a <application> -p <input problem> -f <pattern> -n <arch> [-b 'toggle boundary limit'] [-c 'toggle core binding'] [-g <0/1 enable/disable plotting>] [-r 'toggle scaling with MPI'] [-w 'toggle weak scaling']
+	echo "Usage ./scaling.sh -a <application> -p <input problem> -f <pattern> -n <arch> [-b 'toggle boundary limit'] [-c 'toggle core binding'] [-g 'toggle gpu'] [-s 'toggle pattern size list'] [-t 'toggle throughput plots'] [-w 'toggle weak scaling'] [-x 'toggle plotting']
 		-a : Specify the name of the application (see available apps in the patterns sub-directory)
 		-p : Specify the name of the input problem (see available problems in the application sub-directory)
 		-f : Specify the base name of the input JSON file containing the gather/scatter pattern (see available patterns in the input problem subdirectory)
 		-n : Specify the test name you would like to use to identify this run (used to appropriately save data)
-		-b : Optional, toggle boundary limit on pattern with boundarylist (default: off for weak scaling, on for strong scaling)
+		-b : Optional, toggle boundary limit on pattern with boundarylist (default: off for weak scaling, will be overrridden to on for strong scaling)
 		-c : Optional, toggle core binding (default: off)
-		-g : Optional, toggle plotting/post-processing. Requires python3 environment with pandas, matplotlib (default: on)
-		-r : Optional, enable scaling with MPI (default: off)
-		-w : Optional, enable weak scaling (default: off, strong scaling)"	
+		-g : Optional, toggle gpu (default: off/cpu)
+		-s : Optional, toggle pattern size limit on pattern with sizelist (default: off for weak scaling, will be overridden to on for strong scaling)
+		-t : Optional, toggle throughput plot generation (optional, default: off)
+		-w : Optional, enable weak scaling (default: off, strong scaling)
+		-x : Optional, toggle plotting/post-processing. Requires python3 environment with pandas, matplotlib (default: on)"
 }
 
 if [ $# -lt 3 ] ; then
@@ -18,11 +20,13 @@ if [ $# -lt 3 ] ; then
 else
         SCALINGDIR="spatter.scaling"
         WEAKSCALING=0
-	MPI=0
 	PLOT=1
 	BINDING=0
 	BOUNDARY=0
-	while getopts "a:f:p:n:bcgrw" opt; do
+	PSIZE=0
+	GPU=0
+	THROUGHPUT=0
+	while getopts "a:f:p:n:bcgstwx" opt; do
 		case $opt in 
 			a) APP=$OPTARG
 			;;
@@ -36,49 +40,80 @@ else
 			;;
 			c) BINDING=1
 			;;
-			g) PLOT=$OPTARG
+			g) GPU=1
 			;;
-			r) MPI=1
+			s) PSIZE=1
+			;;
+			t) THROUGHPUT=1
 			;;
 			w) WEAKSCALING=1
+			;;
+			x) PLOT=0
 			;;
 			h) usage; exit 1
 			;;
 		esac
 	done	
 
-	source ./scripts/config.sh
+	if [[ "${GPU}" -eq "1" ]]; then
+		source ./scripts/gpu_config.sh
+	else
+		source ./scripts/cpu_config.sh
+	fi
+
+	if [[ "${PLOT}" -ne "1" ]]; then
+		echo "Plotting disabled, turning off throughput plots"
+		THROUGHPUT=0
+	fi	
+
+	export OMP_NUM_THREADS=1
 	
 	echo "Running scaling.sh:"
 	echo "APP: ${APP}"
 	echo "PROBLEM: ${PROBLEM}"
 	echo "PATTERN: ${PATTERN}"
 	echo "RUNNAME: ${RUNNAME}"
+	echo "GPU: ${GPU}"
 	echo "PLOTTING: ${PLOT}"
-
-	echo "MPI SCALING: ${MPI}"
-	if [[ "${MPI}" -eq "1" ]]; then
-		if [[ "${WEAKSCALING}" -ne "1" ]]; then
-			BOUNDARY=1
-		fi
-
-        	if [[ "${WEAKSCALING}" -eq "1" ]]; then
-                	echo "Weak Scaling"	
-			echo "BOUNDARY: ${BOUNDARY}"
-			if [[ "${BOUNDARY}" -eq "1" ]]; then
-				echo "BOUNDARY LIST: ${boundarylist[0]}"
-			fi
-                        SCALINGDIR="spatter.weakscaling"
-                else
-                	echo "Strong Scaling"
-			echo "BOUNDARY: ${BOUNDARY}"
-			echo "BOUNDARY LIST: ${boundarylist[*]}"
-			echo "PATTERN SIZE LIST: ${sizelist[*]}"
-			SCALINGDIR="spatter.strongscaling"
-		fi
-		echo "RANK LIST: ${ranklist[*]}"
-		echo "BINDING: ${BINDING}"
+	if [[ "${PLOT}" -ne "1" ]]; then
+		echo "Plotting disabled, turning off throughput plots"
+		THROUGHPUT=0
 	fi
+	echo "THROUGHPUT PLOTS: ${THROUGHPUT}"
+
+	if [[ "${WEAKSCALING}" -ne "1" ]]; then
+		echo "Strong Scaling run, turning on boundary size and pattern size"
+		BOUNDARY=1
+		PSIZE=1
+	fi
+
+        if [[ "${WEAKSCALING}" -eq "1" ]]; then
+               	echo "Weak Scaling"	
+		echo "BOUNDARY: ${BOUNDARY}"
+		if [[ "${BOUNDARY}" -eq "1" ]]; then
+			echo "BOUNDARY LIST: ${boundarylist[*]}"
+		fi
+		echo "PATTERN SIZE: ${PSIZE}"
+		if [[ "${PSIZE}" -eq "1" ]]; then
+			echo "PATTERN SIZE LIST: ${sizelist[*]}"
+		fi
+		SCALINGDIR="spatter.weakscaling"
+	else
+               	echo "Strong Scaling"
+		echo "BOUNDARY: ${BOUNDARY}"
+		echo "BOUNDARY LIST: ${boundarylist[*]}"
+		echo "PATTERN SIZE: ${PSIZE}"
+		echo "PATTERN SIZE LIST: ${sizelist[*]}"
+		SCALINGDIR="spatter.strongscaling"
+	fi
+
+	if [[ "${GPU}" -eq "1" ]]; then
+		echo "GPU Run, turning off core binding"
+		BINDING=0
+	fi
+
+	echo "RANK LIST: ${ranklist[*]}"
+	echo "BINDING: ${BINDING}"
 	echo ""
 
 	cd ${HOMEDIR}
@@ -96,63 +131,83 @@ else
 			
 	cd ${SCALINGDIR}/${RUNNAME}/${APP}/${PROBLEM}/${PATTERN}
 
-	if [[ "${MPI}" -eq "1" ]]; then
-		export OMP_NUM_THREADS=1
-		for i in ${!ranklist[*]}; do
-			if [[ "${BINDING}" -eq "1" ]]; then	
-				CMD="mpirun -n ${ranklist[i]} --bind-to=core ${SPATTER} -pFILE=${JSON} -q3"
-				cp ${JSON} ${JSON}.orig
-				if [[ "${WEAKSCALING}" -ne "1" ]]; then
-					sed -Ei 's/\}/\, "pattern-size": '${sizelist[i]}'\}/g' ${JSON}
-					if [[ "${BOUNDARY}" -eq "1" ]]; then
-						sed -Ei 's/\}/\, "boundary": '${boundarylist[i]}'\}/g' ${JSON}
-					fi
-				else
-					if [[ "${BOUNDARY}" -eq "1" ]]; then
-						sed -Ei 's/\}/\, "boundary": '${boundarylist[0]}'\}/g' ${JSON}
-					fi
-				fi
+	for i in ${!ranklist[*]}; do
+		SFILE=""
+		BFILE=""
+
+		# Core Binding
+		if [[ "${BINDING}" -eq "1" ]]; then	
+			CMD="mpirun -n ${ranklist[i]} --bind-to=core ${SPATTER} -pFILE=${JSON} -q3"
+			cp ${JSON} ${JSON}.orig
+			
+			# Strong Scaling
+			if [[ "${WEAKSCALING}" -ne "1" ]]; then
+				SFILE="_${sizelist[i]}s"
+				BFILE="_${boundarylist[i]}b"
+				sed -Ei 's/\}/\, "pattern-size": '${sizelist[i]}'\}/g' ${JSON}
+				sed -Ei 's/\}/\, "boundary": '${boundarylist[i]}'\}/g' ${JSON}
+			# Weak Scaling
 			else
-				CMD="mpirun -n ${ranklist[i]} ${SPATTER} -pFILE=${JSON} -q3"
-				cp ${JSON} ${JSON}.orig
-				if [[ "${WEAKSCALING}" -ne "1" ]]; then
+				if [[ "${PSIZE}" -eq "1" ]]; then
+					SFILE="_${sizelist[i]}s"
 					sed -Ei 's/\}/\"pattern-size": '${sizelist[i]}'\}/g' ${JSON}
-					if [[ "${BOUNDARY}" -eq "1" ]]; then
-						sed -Ei 's/\}/\"boundary": '${boundarylist[i]}'\}\g' ${JSON}
-					fi
-				else	
-					if [[ "${BOUNDARY}" -eq "1" ]]; then
-						sed -Ei 's/\}/\"boundary": '${boundarylist[0]}'\}\g' ${JSON}
-					fi
+				fi
+
+				if [[ "${BOUNDARY}" -eq "1" ]]; then
+					BFILE="_${boundarylist[i]}b"
+					sed -Ei 's/\}/\, "boundary": '${boundarylist[i]}'\}/g' ${JSON}
 				fi
 			fi
+		# No Core Binding
+		else
+			CMD="mpirun -n ${ranklist[i]} ${SPATTER} -pFILE=${JSON} -q3"
+			cp ${JSON} ${JSON}.orig
 
-			echo ${CMD}
-			${CMD} > mpi_${ranklist[i]}r_1t.txt
+			# Strong Scaling
+			if [[ "${WEAKSCALING}" -ne "1" ]]; then
+				SFILE="_${sizelist[i]}s"
+				BFILE="_${boundarylist[i]}b"
+				sed -Ei 's/\}/\"pattern-size": '${sizelist[i]}'\}/g' ${JSON}
+				sed -Ei 's/\}/\"boundary": '${boundarylist[i]}'\}\g' ${JSON}
+			# Weak Scaling
+			else	
+				if [[ "${PSIZE}" -eq "1" ]]; then
+					SFILE="_${sizelist[i]}s"
+					sed -Ei 's/\}/\"pattern-size": '${sizelist[i]}'\}/g' ${JSON}
+				fi
 
-			mv ${JSON}.orig ${JSON}
-
-			mkdir -p ${ranklist[i]}r
-
-			num_patterns=`cat ${JSON} | grep -o -P 'kernel.{0,20}' | wc -l`
-			num_patterns="$((num_patterns-1))"
-
-			for pattern in $(seq 0 ${num_patterns}); do
-				cat mpi_${ranklist[i]}r_1t.txt | grep "^${pattern} " | awk '{print $3}' > ${ranklist[i]}r/${ranklist[i]}r_1t_${pattern}p.txt.tmp
-				cat ${ranklist[i]}r/${ranklist[i]}r_1t_${pattern}p.txt.tmp | awk '{$1=$1};1' > ${ranklist[i]}r/${ranklist[i]}r_1t_${pattern}p.txt
-				rm ${ranklist[i]}r/${ranklist[i]}r_1t_${pattern}p.txt.tmp
-			done
-		done
-
-		echo ""
-	
-		if [[ "${PLOT}" -eq "1" ]]; then
-			cd ${HOMEDIR}
-			mkdir -p figures/${RUNNAME}/${APP}/${PROBLEM}/${PATTERN}
-			echo "Plotting Results..."
-			python3 scripts/plot_mpi.py ${SCALINGDIR}/${RUNNAME}/${APP}/${PROBLEM}/${PATTERN} ${RUNNAME}
-			echo ""
+				if [[ "${BOUNDARY}" -eq "1" ]]; then
+					BFILE="_${boundarylist[i]}b"
+					sed -Ei 's/\}/\"boundary": '${boundarylist[i]}'\}\g' ${JSON}
+				fi
+			fi
 		fi
+
+		echo ${CMD}
+		${CMD} > mpi_${ranklist[i]}r${SFILE}${BFILE}.txt
+
+		mv ${JSON}.orig ${JSON}
+
+		mkdir -p ${ranklist[i]}r
+
+		num_patterns=`cat ${JSON} | grep -o -P 'kernel.{0,20}' | wc -l`
+		num_patterns="$((num_patterns-1))"
+
+		for pattern in $(seq 0 ${num_patterns}); do
+			cat mpi_${ranklist[i]}r${SFILE}${BFILE}.txt | grep "^${pattern} " | awk '{print $3}' > ${ranklist[i]}r/${ranklist[i]}r${SFILE}${BFILE}_${pattern}p.txt.tmp
+			cat ${ranklist[i]}r/${ranklist[i]}r${SFILE}${BFILE}_${pattern}p.txt.tmp | awk '{$1=$1};1' > ${ranklist[i]}r/${ranklist[i]}r${SFILE}${BFILE}_${pattern}p.txt
+			rm ${ranklist[i]}r/${ranklist[i]}r${SFILE}${BFILE}_${pattern}p.txt.tmp
+		done
+	done
+
+	echo ""
+	
+	if [[ "${PLOT}" -eq "1" ]]; then
+		cd ${HOMEDIR}
+		mkdir -p figures/${RUNNAME}/${APP}/${PROBLEM}/${PATTERN}
+		echo "Plotting Results..."
+		python3 scripts/plot_mpi.py ${SCALINGDIR}/${RUNNAME}/${APP}/${PROBLEM}/${PATTERN} ${RUNNAME} ${THROUGHPUT}
+		echo ""
 	fi
 
 	echo "See ${SCALINGDIR}/${RUNNAME}/${APP}/${PROBLEM}/${PATTERN} for results"
